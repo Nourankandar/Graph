e15dd301f51ba47a0a8784064318b8878be18d92
NouranKandar <nourankandar@gmail.com>
2024-07-20 21:25:28 +0300

    delete graphList

--- Main.java
+++ Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Graph2D graph = new Graph2D(5);
-        GraphList graphList = new GraphList();
+       
 
-        graphList.addNode(new Node('A'));
-        graphList.addNode(new Node('B'));
-        graphList.addNode(new Node('C'));
-        graphList.addNode(new Node('D'));
-        graphList.addNode(new Node('E'));
 
         graph.addNode(new Node('A'));
         graph.addNode(new Node('B'));
@@ -24,19 +19,12 @@ public class Main {
         graph.addEdge(4, 0);
         graph.addEdge(4, 2);
 
-        graphList.addEdge(0, 1);
-        graphList.addEdge(1, 2);
-        graphList.addEdge(1, 4);
-        graphList.addEdge(2, 3);
-        graphList.addEdge(2, 4);
-        graphList.addEdge(4, 0);
-        graphList.addEdge(4, 2);
+
         
         graph.print();
 
         System.out.println();
 
-        graphList.print();
 
         graph.Topological_Sort(graph.matrix, 5);
 
