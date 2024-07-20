3eef31181c29a265b779ec224e87f136874597bc
NouranKandar <nourankandar@gmail.com>
2024-07-20 21:35:50 +0300

    hello

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ;
-        GraphList graphList = new GraphList();
-        Graph2D graph = new Graph2D(5);
+        
        
 
 
